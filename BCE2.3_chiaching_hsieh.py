77b429c81bb209be9a345c3469ded26dd79d1eb2
Joyce <chiaching.hsieh@cgu.edu>
2021-09-08 15:16:26 -0700

    upload_file_1

---
+++ BCE2.3_chiaching_hsieh.py
@@ -0,0 +1,9 @@
+# Information
+
+name="Chiaching, Hsieh"
+nick_name="Joyce"
+Favorite_food="hotpot"
+Favorite_movie="Zootopia"
+print(f'Hi, My name is {name} please call me {nick_name}. My favorite food is {Favorite_food} and I love to watch movie/Netflix when I have time.')
+
+
