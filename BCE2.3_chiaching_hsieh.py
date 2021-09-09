a14306733799492a31dcdcf4e09e54e7a3874a55
Joyce <chiaching.hsieh@cgu.edu>
2021-09-09 11:06:51 -0700

    Add new team member information

--- BCE2.3_chiaching_hsieh.py
+++ BCE2.3_chiaching_hsieh.py
@@ -7,3 +7,6 @@ Favorite_movie="Zootopia"
 print(f'Hi, My name is {name} please call me {nick_name}. My favorite food is {Favorite_food} and I love to watch movie/Netflix when I have time.')
 
 
+team_member_1="Jason_Huang"
+nick_name="Jason"
+print(f'Group1 has team member {name} and {team_member_1}')
